7991ea56cb84fc541f9f0f2c78171a22d8bbdd03
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-04-19 23:07:46 +0545

    footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const Footer = () => {
     return (
-        <footer className="bg-white dark:bg-gray-900 mt-4">
+        <footer className="bg-white mt-4">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
@@ -16,17 +16,17 @@ const Footer = () => {
                                 className="h-8 me-3"
                                 alt="FlowBite Logo"
                             />
-                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                            <span className="self-center text-2xl font-semibold whitespace-nowrap text-slate-700">
                                 BidZone
                             </span>
                         </Link>
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
                                 Resources
                             </h2>
-                            <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                            <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <Link
                                         to="https://flowbite.com/"
@@ -46,10 +46,10 @@ const Footer = () => {
                             </ul>
                         </div>
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
                                 Follow us
                             </h2>
-                            <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                            <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <Link
                                         to="https://github.com/themesberg/flowbite"
@@ -69,10 +69,10 @@ const Footer = () => {
                             </ul>
                         </div>
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
                                 Legal
                             </h2>
-                            <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                            <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <Link to="#" className="hover:underline">
                                         Privacy Policy
@@ -87,9 +87,9 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <hr className="my-6 border-gray-700 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                    <span className="text-sm text-gray-500 sm:text-center">
                         © 2024{" "}
                         <Link to="https://flowbite.com/" className="hover:underline">
                             BidZone
@@ -99,7 +99,7 @@ const Footer = () => {
                     <div className="flex mt-4 sm:justify-center sm:mt-0">
                         <Link
                             to="#"
-                            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                            className="text-gray-500 hover:text-gray-900"
                         >
                             <svg
                                 className="w-4 h-4"
@@ -118,7 +118,7 @@ const Footer = () => {
                         </Link>
                         <Link
                             to="#"
-                            className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+                            className="text-gray-500 hover:text-gray-900 ms-5"
                         >
                             <svg
                                 className="w-4 h-4"
@@ -133,7 +133,7 @@ const Footer = () => {
                         </Link>
                         <Link
                             to="#"
-                            className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+                            className="text-gray-500 hover:text-gray-900 ms-5"
                         >
                             <svg
                                 className="w-4 h-4"
@@ -152,7 +152,7 @@ const Footer = () => {
                         </Link>
                         <Link
                             to="#"
-                            className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+                            className="text-gray-500 hover:text-gray-900 ms-5"
                         >
                             <svg
                                 className="w-4 h-4"
@@ -171,7 +171,7 @@ const Footer = () => {
                         </Link>
                         <Link
                             to="#"
-                            className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+                            className="text-gray-500 hover:text-gray-900 ms-5"
                         >
                             <svg
                                 className="w-4 h-4"
