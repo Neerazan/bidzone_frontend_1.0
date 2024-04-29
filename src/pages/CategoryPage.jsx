f0db89b024358a139fec4ec473238fb15bba5bd8
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-04-29 09:06:23 +0545

    Range filter completed

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -4,6 +4,16 @@ import { Range, NumberInput, Container, ProductCard } from "../components/index"
 const CategoryPage = () => {
     const [maxPriceInputValue, setMaxPriceInputValue] = useState()  
     const [minPriceInputValue, setMinPriceInputValue] = useState() 
+    const [maxBidInputValue, setMaxBidInputValue] = useState()
+    const [minBidInputValue, setMinBidInputValue] = useState()
+
+    const HandleMinBidINputChange = (value) => {
+        setMinBidInputValue(value)
+    }
+
+    const HandleMaxBidInputChange = (value) => {
+        setMaxBidInputValue(value)
+    }
 
     const HandleMaxPriceInputChange = (value) => {
         setMaxPriceInputValue(value)
@@ -15,8 +25,9 @@ const CategoryPage = () => {
 
     return (
         <Container>
-            <div className="grid grid-cols-4 gap-4">
-                <div className="col-span-1">
+            <div className="grid grid-cols-4 gap-4 mt-4">
+                <div className="col-span-1 border border-r-gray-300 p-2">
+                    <h2 className="text-gray-600 mb-4">Filter</h2>
                     <div className="flex flex-col gap-3">
                         <div>
                             <p className="text-gray-700 font-semibold text-sm mb-2">
@@ -70,14 +81,65 @@ const CategoryPage = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="w-full border border-b-gray-400 mt-4"></div>
+                    <div className="flex flex-col gap-3 mt-4">
+                        <div>
+                            <p className="text-gray-700 font-semibold text-sm mb-2">
+                                Bids Range
+                            </p>
+
+                            <div className="flex w-9/12">
+                                <Range
+                                    min={1}
+                                    max={3000}
+                                    onInputChange={HandleMinBidINputChange}
+                                    value={minBidInputValue}
+                                    className="w-full"
+                                />
+                                <div className="w-9"></div>
+                                <Range
+                                    min={1}
+                                    max={3000}
+                                    onInputChange={HandleMaxBidInputChange}
+                                    value={maxBidInputValue}
+                                    className="w-full"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="font-semibold text-gray-700 text-sm flex mb-1">
+                                <div className="w-2/5">From</div>
+                                <div className="w-1/2">To</div>
+                            </div>
+                            <div className="flex items-center">
+                                <NumberInput
+                                    onInputChange={HandleMinBidINputChange}
+                                    value={minBidInputValue}
+                                    className="w-full my-auto"
+                                    placeholder="Min"
+                                />
+
+                                <span className="font-bold text-gray-700 mx-[2px]">
+                                    -
+                                </span>
+
+                                <NumberInput
+                                    onInputChange={HandleMaxBidInputChange}
+                                    value={maxBidInputValue}
+                                    className="w-full my-auto"
+                                    placeholder="Max"
+                                />
+
+                                <button className="text-white bg-green-500 font-semibold hover:bg-green-600 text-xs px-4 py-2 ml-1 rounded-sm">
+                                    Apply
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="col-span-3"></div>
             </div>
         </Container>
-        // <div className="ml-12 mt-12">
-        //     <Range min={1} max={1000000} onInputChange={HandleInputChange} value={inputValue}/>
-        //     <NumberInput onInputChange={HandleInputChange} value={inputValue}/>
-        // </div>
     )
 }
 
