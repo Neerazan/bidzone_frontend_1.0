0fade7dbbc828de0a16809a0fbc78e56d868b986
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-04-30 13:23:31 +0545

    Button component

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,36 +1,36 @@
-import React, { useState } from "react"
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-import { Container } from "../index"
+import { Container } from "../index";
 
 function Login() {
-    const navigate = useNavigate()
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [error, setError] = useState(null)
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
 
-    const login = async (e) => {
-        e.preventDefault()
-        const userCredentials = { username, password }
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const userCredentials = { username, password };
 
         try {
             const response = await axios.post(
                 "http://127.0.0.1:8000/auth/jwt/create/",
                 userCredentials
-            )
-            const { access, refresh } = response.data
-            localStorage.setItem("accessToken", JSON.stringify(access))
-            localStorage.setItem("refreshToken", JSON.stringify(refresh))
-            navigate("/")
+            );
+            const { access, refresh } = response.data;
+            localStorage.setItem("accessToken", JSON.stringify(access));
+            localStorage.setItem("refreshToken", JSON.stringify(refresh));
+            navigate("/");
         } catch (error) {
-            setError("Failed to login. Please check your credentials.")
+            setError("Failed to login. Please check your credentials.");
         }
-    }
+    };
 
     return (
         <Container>
-            <form onSubmit={login}>
+            <form onSubmit={handleLogin}>
                 <label>Username: </label>
                 <input
                     type="text"
@@ -50,7 +50,7 @@ function Login() {
             </form>
             {error && <p>{error}</p>}
         </Container>
-    )
+    );
 }
 
-export default Login
+export default Login;
---
+++ src/components/Auth/Loginn.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+
+function Loginn() {
+    return (
+        <div>Loginn</div>
+    )
+}
+
+export default Loginn
