db783f9bfce67771223f71ba2791333a8396f62e
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-04-30 13:34:01 +0545

    Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useId} from 'react'
 
-function Input() {
-    return (
-        <div>Input</div>
+const Input = React.forwardRef( function Input({
+    label,
+    type="text",
+    className="",
+    ...props
+}, ref){
+    const id = useId()
+
+    return(
+        <div className='w-full'>
+            { label&&<label className='inline-block mb-1 pl-1' htmlFor={id}>{label}</label>}
+
+            <input
+                type={type}
+                className={`w-full px-3 py-2  duration-200 borde ${className}`}
+                ref={ref}
+                {...props}
+                id={id}
+            />
+        </div>
     )
-}
+})
 
 export default Input
