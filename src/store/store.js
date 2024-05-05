b208d6df82fe9591984c95eb98fedb51c2279b21
Neerajan Dhakal <86935895+Neerazan@users.noreply.github.com>
2024-05-05 12:18:38 +0545

    Balance Reducer and store made

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice"
 import collectionReducer from "./common/collectionSlice";
 import wishlistReducer from "./common/wishlistSlice";
+import BalanceReducer from "./common/BalanceSlice";
 
 const store = configureStore({
     reducer :{
         auth: authReducer,
         collection: collectionReducer,
         wishlist: wishlistReducer,
+        balance: BalanceReducer
     },
 })
 
